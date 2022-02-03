4027418fdce460dabf7be0dc00d251d25b168f94
angie <amaticorena103@hotmail.com>
2022-02-02 22:25:40 -0500

    commit tree 425429c44a3ec7211ee7e31089639134b6abe280

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants, currentSearch}) {
+
+    const filteredPlants = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(currentSearch.toLowerCase())
+    })
+     
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{filteredPlants.map(plant => <PlantCard plant={plant} key={plant.name}/>)}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [currentSearch, setCurrentSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(r => r.json())
+    .then(plants => setPlants(plants))
+  }, [])
+
+  function handlePlantSubmit(newPlant){
+    setPlants([...plants, newPlant])
+  }
+
+  function handleSearch(search){
+    setCurrentSearch(search)
+  }
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onPlantSubmit={handlePlantSubmit}/>
+      <Search onPlantSearch={handleSearch} />
+      <PlantList plants={plants} currentSearch={currentSearch}/>
     </main>
   );
 }
